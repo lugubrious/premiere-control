fac524593e1abe7e85ebb78a08f7def26c328cc8
Samuel Dewan <samdewan@me.com>
2015-11-25 09:43:36 -0500

    Initial Commit

---
+++ Premiere - Control/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Premiere - Control
+//
+//  Created by Samuel Dewan on 2015-11-25.
+//  Copyright © 2015 Samuel Dewan. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
